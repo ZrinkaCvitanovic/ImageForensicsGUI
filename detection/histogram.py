1347ab60780df5209a3ac002819563053dd8c207
ZrinkaCvitanovic <zrinka.cvitanovic11@gmail.com>
2026-03-09 16:04:26 +0100

    added luminence and hsv to detection

--- detection/histogram.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-image = cv2.imread('ela.jpg')
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-equalized_image = cv2.equalizeHist(gray_image)
-
-cv2.imwrite('equalised.jpg',equalized_image)
