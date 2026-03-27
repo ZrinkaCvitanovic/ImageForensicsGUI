1524305ecc7693e5899ec315f894f64dc7e1166b
ZrinkaCvitanovic <zrinka.cvitanovic11@gmail.com>
2026-03-27 09:22:55 +0100

    old changes

--- detection/2.change-color-scheme/hsv.py
+++ detection/2.change-color-scheme/hsv.py
@@ -7,7 +7,6 @@ parser.add_argument('method', metavar='method', type=str, choices=['hsv', 'lum']
                     help="desired method for converting your image")
 args = parser.parse_args()
 original_img = cv2.imread(args.in_path)
-
 if args.method == "hsv":
     result = cv2.cvtColor(original_img, cv2.COLOR_BGR2HSV)
     output_path = args.in_path + "_hsv.png"
