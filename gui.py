820b593fa382af08d3b73dc6556963eb39fffb7b
ZrinkaCvitanovic <zrinka.cvitanovic11@gmail.com>
2026-04-02 14:55:53 +0200

    removed numbers to enable external imports

--- gui.py
+++ gui.py
@@ -1,15 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
+from proxy import *
 
 root = tk.Tk()
 tabControl = ttk.Notebook(root)
 
-tab1 = ttk.Frame(tabControl, relief=tk.GROOVE)
-tab2 = ttk.Frame(tabControl, relief=tk.GROOVE)
-tab3 = ttk.Frame(tabControl, relief=tk.GROOVE)
+tab1 = ttk.Frame(tabControl)
+tab2 = ttk.Frame(tabControl)
+tab3 = ttk.Frame(tabControl)
 tabControl.pack(expand = 1, fill ="both")
 
-def ela():
+def ela(event):
     lbl_ela_msg["text"] = ""
     in_image = ent_ela_in.get()
     try:
@@ -23,8 +24,7 @@ def ela():
     finally:
         ent_ela_in.delete(0, tk.END)
         ent_ela_q.delete(0, tk.END)
-    
-    
+        proxy_ela(ent_ela_in.get(), ent_ela_q.get())
     
     
 
@@ -40,7 +40,8 @@ ent_ela_in = tk.Entry(tab2, width=100)
 ent_ela_in.grid(row=1, column=1, sticky="w")
 ent_ela_q = tk.Entry(tab2, width=5)
 ent_ela_q.grid(row=2, column=1, sticky="w")
-btn_ela = tk.Button(tab2, text='Start', command=ela)
+btn_ela = tk.Button(tab2, text='Start')
+btn_ela.bind('<Button>', ela)
 btn_ela.grid(row=3, column=1, sticky="w")
 lbl_ela_msg = tk.Label(tab2, text="")
 lbl_ela_msg.grid(row=4, column=1)
---
+++ proxy.py
@@ -0,0 +1,4 @@
+from detection.ela.main import *
+
+def proxy_ela(img_path, quality):
+    ELA(img_path, quality)
