8a4512a13ec6e0e3fb57686f35c1751b44ed0b8d
Zrinka Cvitanović <zcvitanovic@insig2.com>
2026-04-29 12:04:04 +0200

    updated edge detection script

--- detection/edges/test-edges.sh
+++ detection/edges/test-edges.sh
@@ -1,12 +1,23 @@
 #usage: ./test-edges.sh [path/to/image]
 
-python3 main.py --robust $1 30 200
-python3 main.py $1 30 200 
-python3 main.py $1 50 400 
-python3 main.py --robust $1 50 400 
-python3 main.py $1 100 500 
-python3 main.py --robust $1 100 500 
+python3 main.py --robust $1 30 60
+python3 main.py $1 30 60 
+python3 main.py --robust $1 30 90
+python3 main.py $1 30 90 
+python3 main.py --robust $1 50 100
+python3 main.py $1 500 100 
+python3 main.py --robust $1 50 150
+python3 main.py $1 50 150
+python3 main.py --robust $1 100 200
+python3 main.py $1 100 200
+python3 main.py --robust $1 100 300
+python3 main.py $1 100 300
+python3 main.py --robust $1 200 300
+python3 main.py $1 200 300
+python3 main.py --robust $1 200 400
+python3 main.py $1 200 400
 python3 main.py $1 200 600 
-python3 main.py --robust $1 200 600 
+python3 main.py --robust $1 200 600
+
 
 
