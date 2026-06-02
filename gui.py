67306f8f9f5a4c3ccb6e0fa0bfee6171a836d545
ZrinkaCvitanovic <zrinka.cvitanovic11@gmail.com>
2026-06-02 11:29:16 +0200

    added help buttons

--- gui.py
+++ gui.py
@@ -17,28 +17,6 @@ tabControl.pack(expand = 1, fill ="both")
 
 global num_of_images_uploaded
 
-class CustomTooltip:
-    def __init__(self, widget, text):
-        self.widget = widget
-        self.text = text
-        self.widget.bind("<Enter>", self.enter)
-        self.widget.bind("<Leave>", self.leave)
-        self.tooltip_window = None
-    def enter(self, event=None):
-        x, y, cx, cy = self.widget.bbox("insert")
-        x += self.widget.winfo_rootx() + 25
-        y += self.widget.winfo_rooty() + 20
-        self.tooltip_window = tk.Toplevel(self.widget)
-        self.tooltip_window.wm_overrideredirect(True)
-        self.tooltip_window.wm_geometry("+%d+%d" % (x, y))
-        label = tk.Label(self.tooltip_window, text=self.text, justify='left',
-                         background='#ffffff', relief='solid', borderwidth=1,
-                         font=("TkDefaultFont", "12", "normal"))
-        label.pack(ipadx=1)
-    def leave(self, event=None):
-        if self.tooltip_window:
-            self.tooltip_window.destroy()
-
 def browse_file(entry_widget):
     """Generic file browser that inserts selected path into the provided entry widget"""
     filename = filedialog.askopenfilename(
@@ -138,7 +116,7 @@ def event_resize(event):
             ent_resize_height.delete(0, tk.END)
             ent_resize_width.delete(0, tk.END)
     except:
-        lbl_resize_msg["text"] = "Height and width scale values must be numbers!"
+        lbl_resize_msg["text"] = "Height and width scale values must be integers!"
 
 def event_noise(event):
     in_image = ent_noise_in.get()
@@ -193,6 +171,51 @@ def removeImage(event):
     global num_of_images_uploaded
     num_of_images_uploaded -= 1
 
+def displayHelp(method):
+    match method:
+        case "ELA":
+            if len(lbl_ela_help["text"]) == 0:
+                lbl_ela_help["text"] = "Recommended values for quality are between 40 and 80.\nFor high resolution images, choose lower values and vice versa." 
+            else:
+                lbl_ela_help["text"] = ""
+        case "color":
+            print(lbl_color_help["text"])
+            if len(lbl_color_help["text"]) == 0:
+                lbl_color_help["text"] = "Always use both methods because each can cause false positives."
+            else:
+                lbl_color_help["text"] = ""
+        case "edges":
+            if len(lbl_edge_help["text"]) == 0:
+                lbl_edge_help["text"] = """
+                Recommended values for lower threshold are between 40 and 100 for natural images.\nFor darker pictures, the threshold can be higher.\n
+                Higher threshold should be about twice as large as the lower. \nIf many edges are undetected, lower it, but always make it larger than the lower threshold."""
+            else:
+                lbl_edge_help["text"] = ""
+        case "telea":
+            if len(lbl_telea_help["text"]) == 0:
+                lbl_telea_help["text"] = "Default value for radius is 5. For less narrow masks, choose a smaller radius."
+            else:
+                lbl_telea_help["text"] = ""
+        case "resize":
+            if len(lbl_resize_help["text"]) == 0:
+                lbl_resize_help["text"] = """
+            Linear interpolation - fastest, works fine for pictures without too much details
+            Lanzos - smoother, but for a large amount of images can be slow.
+            Cubic - a compromise between the two
+            """
+            else:
+                lbl_resize_help["text"] = ""
+        case "noise":
+            if len(lbl_noise_help["text"]) == 0:
+                lbl_noise_help["text"] = "Use 'gaussian' for Gaussian noise and 'median' for random or 'salt and pepper' noise."
+
+            else:
+                lbl_noise_help["text"] = ""
+        case "sharp":
+             if len(lbl_sharp_msg["text"]) == 0:
+                lbl_sharp_msg["text"] = "Use 'kernel' for detailed images and 'laplacian' for noisy images."
+             else:
+                 lbl_sharp_msg["text"] = ""
 
 
 
@@ -209,7 +232,9 @@ ent_ela_in.grid(row=1, column=1, sticky="w")
 tk.Button(tab1, text="Browse Files",command=partial(browse_file, ent_ela_in)).grid(row=1, column=2, sticky="w")
 ent_ela_q = tk.Entry(tab1, width=5)
 ent_ela_q.grid(row=2, column=1, sticky="w")
-tooltip_ela = CustomTooltip(ent_ela_q, "Recommended values for quality are between 40 and 80.\nFor high resolution images, choose lower values and vice versa.")
+btn_ela_help = tk.Button(tab1, text="Help", command=lambda: displayHelp("ELA")).grid(row=1, column=3, sticky="w")
+lbl_ela_help = tk.Label(tab1, text="")
+lbl_ela_help.grid(row=5, column=1)
 btn_ela = tk.Button(tab1, text='Start')
 btn_ela.bind('<Button>', event_ela)
 btn_ela.grid(row=3, column=1, sticky="w")
@@ -227,12 +252,14 @@ tk.Button(tab1, text="Browse Files",command=partial(browse_file, ent_color_in)).
 opt_color_method = tk.StringVar(value="hsv")
 opt_color_dropdown = tk.OptionMenu(tab1, opt_color_method, "hsv", "lum")
 opt_color_dropdown.grid(row=12, column=1, sticky="w")
-tooltip_color = CustomTooltip(opt_color_dropdown, "Always use both methods because each can cause false positives.")
 btn_color = tk.Button(tab1, text='Start')
 btn_color.bind('<Button>', event_color)
 btn_color.grid(row=13, column=1, sticky="w")
 lbl_color_msg = tk.Label(tab1, text="")
 lbl_color_msg.grid(row=14, column=1)
+btn_color_help = tk.Button(tab1, text="Help", command=lambda: displayHelp("color")).grid(row=11, column=3, sticky="w")
+lbl_color_help = tk.Label(tab1, text="")
+lbl_color_help.grid(row=15, column=1)
 
 lbl_edge_title = tk.Label(tab1, text ="Edge detection")
 lbl_edge_title.config(font=("TkDefaultFont", 10, "bold"))
@@ -245,10 +272,11 @@ ent_edge_in.grid(row=21, column=1, sticky="w")
 tk.Button(tab1, text="Browse Files",command=partial(browse_file, ent_edge_in)).grid(row=21, column=2, sticky="w")
 ent_edge_lower = tk.Entry(tab1, width=5)
 ent_edge_lower.grid(row=22, column=1, sticky="w")
-tooltip_lower = CustomTooltip(ent_edge_lower, "Recommended values for lower threshold are between 40 and 100 for natural images.\nFor darker pictures, the threshold can be higher.")
+btn_edge_help = tk.Button(tab1, text="Help", command=lambda: displayHelp("edges")).grid(row=21, column=3, sticky="w")
+lbl_edge_help = tk.Label(tab1, text="")
+lbl_edge_help.grid(row=28, column=1)
 ent_edge_higher = tk.Entry(tab1, width=5)
 ent_edge_higher.grid(row=23, column=1, sticky="w")
-tooltip_higher = CustomTooltip(ent_edge_higher, "Start with a value twice as large as the lower threshold. \nIf many edges are undetected, lower it, but always make it larger than the lower threshold.")
 btn_edge = tk.Button(tab1, text='Start')
 btn_edge.bind('<Button>', event_edge)
 btn_edge.grid(row=26, column=1, sticky="w")
@@ -273,7 +301,9 @@ tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_telea_mask)
 ent_telea_radius = tk.Entry(tab2, width=5)
 ent_telea_radius.grid(row=3, column=1, sticky="w")
 ent_telea_radius.insert(0, 5)
-tooltip_telea = CustomTooltip(ent_telea_radius, "Default value for radius is 5. For less narrow masks, choose a smaller radius.")
+btn_telea_help = tk.Button(tab2, text="Help", command=lambda: displayHelp("telea")).grid(row=1, column=3, sticky="w")
+lbl_telea_help = tk.Label(tab2, text="")
+lbl_telea_help.grid(row=6, column=1)
 btn_telea = tk.Button(tab2, text='Start')
 btn_telea.bind('<Button>', event_telea)
 btn_telea.grid(row=4, column=1, sticky="w")
@@ -282,99 +312,103 @@ lbl_telea_msg.grid(row=5, column=1)
 
 lbl_patch_title = tk.Label(tab2, text ="PatchMatch inpainting")
 lbl_patch_title.config(font=("TkDefaultFont", 10, "bold"))
-lbl_patch_title.grid(row=10, column=0, sticky="w")
-tk.Label(tab2, text="Input image", width=50).grid(row=11, column=0, sticky="w")
-tk.Label(tab2, text="Mask image", width=50).grid(row=12, column=0, sticky="w")
-tk.Label(tab2, text="Mask color", width=50).grid(row=13, column=0, sticky="w")
+lbl_patch_title.grid(row=7, column=0, sticky="w")
+tk.Label(tab2, text="Input image", width=50).grid(row=8, column=0, sticky="w")
+tk.Label(tab2, text="Mask image", width=50).grid(row=9, column=0, sticky="w")
+tk.Label(tab2, text="Mask color", width=50).grid(row=10, column=0, sticky="w")
 ent_patch_in = tk.Entry(tab2, width=100)
-ent_patch_in.grid(row=11, column=1, sticky="w")
-tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_patch_in)).grid(row=11, column=2, sticky="w")
+ent_patch_in.grid(row=8, column=1, sticky="w")
+tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_patch_in)).grid(row=9, column=2, sticky="w")
 ent_patch_mask = tk.Entry(tab2, width=100)
-ent_patch_mask.grid(row=12, column=1, sticky="w")
-tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_patch_mask)).grid(row=12, column=2, sticky="w")
+ent_patch_mask.grid(row=9, column=1, sticky="w")
+tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_patch_mask)).grid(row=9, column=2, sticky="w")
 opt_patch_maskc = tk.StringVar(value="white")
 opt_patch_dropdown = tk.OptionMenu(tab2, opt_patch_maskc, "white", "black", "red", "green", "blue")
-opt_patch_dropdown.grid(row=13, column=1, sticky="w")
+opt_patch_dropdown.grid(row=10, column=1, sticky="w")
 btn_patch = tk.Button(tab2, text='Start')
 btn_patch.bind('<Button>', event_patch)
-btn_patch.grid(row=15, column=1, sticky="w")
+btn_patch.grid(row=11, column=1, sticky="w")
 lbl_patch_msg = tk.Label(tab2, text="")
-lbl_patch_msg.grid(row=16, column=1)
+lbl_patch_msg.grid(row=12, column=1)
 
 lbl_resize_title = tk.Label(tab2, text ="Image resizing")
 lbl_resize_title.config(font=("TkDefaultFont", 10, "bold"))
-lbl_resize_title.grid(row=20, column=0, sticky="w")
-tk.Label(tab2, text="Input image", width=50).grid(row=21, column=0, sticky="w")
-tk.Label(tab2, text="Height scale", width=50).grid(row=22, column=0, sticky="w")
-tk.Label(tab2, text="Width scale", width=50).grid(row=23, column=0, sticky="w")
-tk.Label(tab2, text="Method", width=50).grid(row=24, column=0, sticky="w")
+lbl_resize_title.grid(row=13, column=0, sticky="w")
+tk.Label(tab2, text="Input image", width=50).grid(row=14, column=0, sticky="w")
+tk.Label(tab2, text="Height scale", width=50).grid(row=15, column=0, sticky="w")
+tk.Label(tab2, text="Width scale", width=50).grid(row=16, column=0, sticky="w")
+tk.Label(tab2, text="Method", width=50).grid(row=17, column=0, sticky="w")
 ent_resize_in = tk.Entry(tab2, width=100)
-ent_resize_in.grid(row=21, column=1, sticky="w")
-tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_resize_in)).grid(row=21, column=2, sticky="w")
+ent_resize_in.grid(row=14, column=1, sticky="w")
+tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_resize_in)).grid(row=14, column=2, sticky="w")
 ent_resize_height = tk.Entry(tab2, width=5)
-ent_resize_height.grid(row=22, column=1, sticky="w")
-tooltip_height = CustomTooltip(ent_resize_height, "Choose how much you want to increase height of the image (enter 1 for no change).\nDon't use decimal numbers.")
+ent_resize_height.grid(row=15, column=1, sticky="w")
 ent_resize_width = tk.Entry(tab2, width=5)
-ent_resize_width.grid(row=23, column=1, sticky="w")
-tooltip_height = CustomTooltip(ent_resize_width, "Choose how much you want to increase width of the image (enter 1 for no change).\nDon't use decimal numbers.")
+ent_resize_width.grid(row=16, column=1, sticky="w")
+btn_resize_help = tk.Button(tab2, text="Help", command=lambda: displayHelp("resize")).grid(row=14, column=3, sticky="w")
+lbl_resize_help = tk.Label(tab2, text="")
+lbl_resize_help.grid(row=20, column=1)
 opt_resize_method = tk.StringVar(value="cubic")
 opt_resize_dropdown = tk.OptionMenu(tab2, opt_resize_method, "cubic", "lanzos", "linear")
-opt_resize_dropdown.grid(row=24, column=1, sticky="w")
-tooltip_height = CustomTooltip(opt_resize_dropdown, "Linear interpolation is the fastest and works fine for pictures without too much details.\nFor a smoother interpolation, choose 'lanzos'.\nFor a large amount of images, Lanzos interpolation can cause the program to work slow.")
+opt_resize_dropdown.grid(row=17, column=1, sticky="w")
 btn_resize = tk.Button(tab2, text='Start')
 btn_resize.bind('<Button>', event_resize)
-btn_resize.grid(row=25, column=1, sticky="w")
+btn_resize.grid(row=18, column=1, sticky="w")
 lbl_resize_msg = tk.Label(tab2, text="")
-lbl_resize_msg.grid(row=26, column=1)
+lbl_resize_msg.grid(row=19, column=1)
 
 lbl_noise_title = tk.Label(tab2, text ="Removing noise from images")
 lbl_noise_title.config(font=("TkDefaultFont", 10, "bold"))
-lbl_noise_title.grid(row=30, column=0, sticky="w")
-tk.Label(tab2, text="Input image", width=50).grid(row=31, column=0, sticky="w")
-tk.Label(tab2, text="Method", width=50).grid(row=32, column=0, sticky="w")
+lbl_noise_title.grid(row=21, column=0, sticky="w")
+tk.Label(tab2, text="Input image", width=50).grid(row=22, column=0, sticky="w")
+tk.Label(tab2, text="Method", width=50).grid(row=23, column=0, sticky="w")
 ent_noise_in = tk.Entry(tab2, width=100)
-ent_noise_in.grid(row=31, column=1, sticky="w")
-tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_noise_in)).grid(row=31, column=2, sticky="w")
+ent_noise_in.grid(row=22, column=1, sticky="w")
+tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_noise_in)).grid(row=22, column=2, sticky="w")
 opt_noise_method = tk.StringVar(value="gaussian")
 opt_noise_dropdown = tk.OptionMenu(tab2, opt_noise_method, "gaussian", "median")
-tooltip_height = CustomTooltip(opt_noise_dropdown, "Use 'gaussian' for Gaussian noise and 'median' for random or 'salt and pepper' noise.")
-opt_noise_dropdown.grid(row=32, column=1, sticky="w")
+btn_noise_help = tk.Button(tab2, text="Help", command=lambda: displayHelp("noise")).grid(row=22, column=3, sticky="w")
+lbl_noise_help = tk.Label(tab2, text="")
+lbl_noise_help.grid(row=26, column=1)
+opt_noise_dropdown.grid(row=23, column=1, sticky="w")
 btn_noise = tk.Button(tab2, text='Start')
 btn_noise.bind('<Button>', event_noise)
-btn_noise.grid(row=33, column=1, sticky="w")
+btn_noise.grid(row=24, column=1, sticky="w")
 lbl_noise_msg = tk.Label(tab2, text="")
-lbl_noise_msg.grid(row=34, column=1)
+lbl_noise_msg.grid(row=25, column=1)
 
 lbl_contrast_title = tk.Label(tab2, text ="Increasing contrast to images")
 lbl_contrast_title.config(font=("TkDefaultFont", 10, "bold"))
-lbl_contrast_title.grid(row=40, column=0, sticky="w")
-tk.Label(tab2, text="Input image", width=50).grid(row=41, column=0, sticky="w")
+lbl_contrast_title.grid(row=26, column=0, sticky="w")
+tk.Label(tab2, text="Input image", width=50).grid(row=27, column=0, sticky="w")
 ent_contrast_in = tk.Entry(tab2, width=100)
-ent_contrast_in.grid(row=41, column=1, sticky="w")
-tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_contrast_in)).grid(row=41, column=2, sticky="w")
+ent_contrast_in.grid(row=27, column=1, sticky="w")
+tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_contrast_in)).grid(row=27, column=2, sticky="w")
 btn_contrast = tk.Button(tab2, text='Start')
 btn_contrast.bind('<Button>', event_contrast)
-btn_contrast.grid(row=43, column=1, sticky="w")
+btn_contrast.grid(row=28, column=1, sticky="w")
 lbl_contrast_msg = tk.Label(tab2, text="")
-lbl_contrast_msg.grid(row=44, column=1)
+lbl_contrast_msg.grid(row=29, column=1)
 
 lbl_sharp_title = tk.Label(tab2, text ="Sharpening images")
 lbl_sharp_title.config(font=("TkDefaultFont", 10, "bold"))
-lbl_sharp_title.grid(row=50, column=0, sticky="w")
-tk.Label(tab2, text="Input image", width=50).grid(row=51, column=0, sticky="w")
-tk.Label(tab2, text="Method", width=50).grid(row=52, column=0, sticky="w")
+lbl_sharp_title.grid(row=30, column=0, sticky="w")
+tk.Label(tab2, text="Input image", width=50).grid(row=31, column=0, sticky="w")
+tk.Label(tab2, text="Method", width=50).grid(row=32, column=0, sticky="w")
 ent_sharp_in = tk.Entry(tab2, width=100)
-ent_sharp_in.grid(row=51, column=1, sticky="w")
-tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_sharp_in)).grid(row=51, column=2, sticky="w")
+ent_sharp_in.grid(row=31, column=1, sticky="w")
+tk.Button(tab2, text="Browse Files",command=partial(browse_file, ent_sharp_in)).grid(row=31, column=2, sticky="w")
 opt_sharp_method = tk.StringVar(value="kernel")
 opt_sharp_dropdown = tk.OptionMenu(tab2, opt_sharp_method, "kernel", "laplacian")
-opt_sharp_dropdown.grid(row=52, column=1, sticky="w")
-tooltip_height = CustomTooltip(opt_sharp_dropdown, "Use 'kernel' for detailed images and 'laplacian' for noisy images.")
+opt_sharp_dropdown.grid(row=32, column=1, sticky="w")
+btn_sharp_help = tk.Button(tab2, text="Help", command=lambda: displayHelp("sharp")).grid(row=31, column=3, sticky="w")
+lbl_sharp_help = tk.Label(tab2, text="")
+lbl_sharp_help.grid(row=35, column=1)
 btn_sharp = tk.Button(tab2, text='Start')
 btn_sharp.bind('<Button>', event_sharp)
-btn_sharp.grid(row=53, column=1, sticky="w")
+btn_sharp.grid(row=33, column=1, sticky="w")
 lbl_sharp_msg = tk.Label(tab2, text="")
-lbl_sharp_msg.grid(row=54, column=1)
+lbl_sharp_msg.grid(row=34, column=1)
 
 num_of_images_uploaded = 0
 tabControl.add(tab3, text ='Compare results')
